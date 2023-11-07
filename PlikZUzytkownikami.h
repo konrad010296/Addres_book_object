95ba9da0971c6c2c2eeebd92dd753dd0909ea508
Konrad <konradskolimowski1996@interia.pl>
2023-11-07 20:35:18 +0100

    Refactor code

--- PlikZUzytkownikami.h
+++ PlikZUzytkownikami.h
@@ -9,11 +9,11 @@ using namespace std;
 
 class PlikZUzytkownikami
 {
-    const string nazwaPlikuZUzytkownikami;
+    const string NAZWA_PLIKU_Z_UZYTKOWNIKAMI;
     string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik &uzytkownik);
     Uzytkownik pobierzDaneUzytkownika(string daneJednegoUzytkownikaOddzielonePionowymiKreskami);
 public:
-    PlikZUzytkownikami(string nazwaPlikuUzytkownicy) : nazwaPlikuZUzytkownikami(nazwaPlikuUzytkownicy) {};
+    PlikZUzytkownikami(string nazwaPlikuUzytkownicy) : NAZWA_PLIKU_Z_UZYTKOWNIKAMI(nazwaPlikuUzytkownicy) {};
     void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
     vector <Uzytkownik> wczytajUzytkownikowZPliku();
     void zapiszWszystkichUzytkownikowDoPliku(vector <Uzytkownik> &uzytkownicy);
